38090682a06dfa04f39fe26643388f27a11207b7
n8jja <jeremy@n8jja.com>
2021-11-04 21:20:12 -0700

    Refactored gps
    - Added some comments for clarity
    - Moved Gps methods to child classes
    - Created empty child class for U-Blox based GPS devices
    - Created class for NMEA-compatible GPS devices
    - Added GPS stream to NMEA GPS class to allow for easy access to raw sentences

--- gps.py
+++ gps.py
@@ -7,10 +7,10 @@ Date: 6/10/2021
 
 """
 
-
 import os
 import serial
 import pynmeagps
+import pyubx2
 from geopy import distance
 
 
@@ -20,6 +20,8 @@ class Gps:
         self.baud_rate = baud
         self.timeout = timeout
 
+    # Get current gridsquare from Winlink Express.
+    # Should be changed to allow configuration of path and program i.e. Pat, etc.)
     def get_current_grid_square(self):
         filepath = os.path.join('C:\\', 'RMS Express', 'RMS Express.ini')
         with open(filepath, 'r') as file:
@@ -28,14 +30,6 @@ class Gps:
         current_gridsquare = lines[26]
         current_gridsquare = current_gridsquare.split('=')
         return current_gridsquare[1]
-
-    # Read GPS coordinates from the GPS
-    def get_gps_coordinates(self):
-        # Open serial connection to the unit.  COM port may be different depending on each system.  Default is COM7
-        stream = serial.Serial(self.com_port, self.baud_rate, self.timeout)
-        nmr = pynmeagps.NMEAReader(stream)
-        (raw_data, parsed_data) = nmr.read()  # raw_data is not used, but is required or error will result.
-        return [round(parsed_data.lat, 6), round(parsed_data.lon, 6)]
 
     # Convert GPS coordinates into grid square
     def convert_to_grid(self, lat, lon):
@@ -55,18 +49,49 @@ class Gps:
 
         # Perform the actual calculation and return
         return upper[int(lon / 20)] + upper[int(lat / 10)] + str(int((lon / 2) % 10)) + str(int(lat % 10)) + \
-            lower[int(lon_remainder / 5)] + lower[int(lat_remainder/2.5)]
+               lower[int(lon_remainder / 5)] + lower[int(lat_remainder / 2.5)]
 
     def convert_to_latlong(self, gridsquare):
         upper = 'ABCDEFGHIJKLMNOPQRSTUVWX'
         lower = 'abcdefghijklmnopqrstuvwx'
 
         gridsquare = list(gridsquare)
-        lat = round(((float(upper.index(gridsquare[1]))) * 10) + float(gridsquare[3]) + (((lower.index(gridsquare[5].lower())) / 24) + (1/48) - 90.0), 6)
-        lon = round(((float(upper.index(gridsquare[0])) * 20) + (float(gridsquare[2]) * 2) + (float(lower.index(gridsquare[4].lower()) / 12) + (1/24))) - 180, 6)
+        lat = round(((float(upper.index(gridsquare[1]))) * 10) + float(gridsquare[3]) + (
+                    ((lower.index(gridsquare[5].lower())) / 24) + (1 / 48) - 90.0), 6)
+        lon = round(((float(upper.index(gridsquare[0])) * 20) + (float(gridsquare[2]) * 2) + (
+                    float(lower.index(gridsquare[4].lower()) / 12) + (1 / 24))) - 180, 6)
 
         return lat, lon
 
     def get_geo_distance(self, user_station, foreign_station):
         return distance.distance(self.convert_to_latlong(user_station), self.convert_to_latlong(foreign_station)).miles
+
+
+# The NmeaGPS class is created in order to connect to and utilize NMEA compatible GPS devices.
+class NmeaGPS(Gps):
+    def __init__(self, com):
+        super().__init__(com)
+        self.stream = serial.Serial(self.com_port, self.baud_rate, self.timeout)
+        self.nmr = pynmeagps.NMEAReader(self.stream)
+
+    # Read GPS coordinates from the GPS unit
+    def get_gps_coordinates(self):
+        # Open serial connection to the unit.  COM port may be different depending on each system.  Default is COM7
+
+        (raw_data, parsed_data) = self.nmr.read()  # raw_data is not used, but is required or error will result.
+        return [round(parsed_data.lat, 6), round(parsed_data.lon, 6)]
+
+    # provide a live stream of raw NMEA GPS sentences.  Can be used for updating position on a map, etc.
+    def gps_stream(self):
+        (raw_data, parsed_data) = self.nmr.read()
+        # There may be more code required...this may not read the GPS sentence specifically.
+        return raw_data
+
+
+# The UbloxGPS class is used to represent U-Blox GPS units.  Available data is  not the same
+# as NMEA compatible GPS units.
+class UbloxGPS(Gps):
+    def __init__(self, com):
+        super().__init__(com)
+
 
